6cd0391c3dcdb976afb73511134b3cee2daa245a
darkentrery <badajkovmihail@gmail.com>
2022-04-17 13:04:19 +0000

    Update robot_positions.py

--- robot_positions.py
+++ robot_positions.py
@@ -16,7 +16,6 @@ class Position():
         self.price_position = float(price_position)
         self.close = float(close)
         self.pnl = (self.close - self.price_position) * self.size_position
-        self.equity = self.balance + self.pnl
         self.rpl = 0
         self.start = True
 
@@ -26,7 +25,7 @@ class Position():
         self.leverage = leverage
         self.price_order = self.close
         if order_leverage > 0:
-            self.size_order = order_leverage * self.equity / self.price_order
+            self.size_order = order_leverage * (self.balance + self.pnl) / self.price_order
             self.price_position += (self.price_order - self.price_position) * (
                         self.size_order / (self.size_position + self.size_order))
         elif order_leverage <= 0 and leverage_0 != 0:
@@ -42,86 +41,87 @@ class Position():
         self.size_position += self.size_order
         self.close = close
 
+        if not leverage:
+            self.start = False
+
 
 
     def update_long(self):
         self.pnl = (self.close - self.price_position) * self.size_position
-        self.equity = self.balance + self.pnl
 
         return self.balance, self.leverage, self.size_order, self.price_order, self.size_position, self.price_position,\
-               self.close, self.pnl, self.equity, self.rpl
+               self.close, self.pnl, self.rpl
 
     def update_short(self):
         self.pnl = (self.price_position - self.close) * self.size_position
-        self.equity = self.balance + self.pnl
 
         return self.balance, self.leverage, self.size_order, self.price_order, self.size_position, self.price_position,\
-               self.close, self.pnl, self.equity, self.rpl
+               self.close, self.pnl, self.rpl
 
 
 
-def update_position(stream, block, candles, equity, pos):
+def update_position(stream, block, candles, position, pos):
     candle = candles[0]
-    stream['order'] = get_params(stream, block, candles, equity, pos)
+    stream['order'] = get_params(stream, block, candles, position, pos)
     pos.db_insert_position(stream, candle, stream['order'])
 
 
-def get_leverage(block, many_params):
+def get_leverage(block, parametrs):
     if 'leverage_up' in block['actions'][0]:
-        leverage = many_params['leverage'] + block['actions'][0]['leverage_up']
+        leverage = parametrs['leverage'] + block['actions'][0]['leverage_up']
     elif 'leverage_down' in block['actions'][0]:
         leverage = 0
     elif 'leverage' in block['actions'][0]:
         leverage = block['actions'][0]['leverage']
     else:
-        leverage = many_params['leverage']
+        leverage = parametrs['leverage']
     return leverage
 
 
 def get_params(stream, block, candles, position, pos):
     candle = candles[0]
     print(f"{candles=}")
-    many_params = pos.get_last_order(stream)
+    parametrs = pos.get_last_order(stream)
     #direction = stream['activation_blocks'][0]['actions'][0]['direction']
     direction = block['actions'][0]['direction']
     print(f"{direction=}")
-
-    if many_params == {}:
-        many_params['balance'] = float(1)
-        many_params['leverage'] = float(0)
-        many_params['price_order'] = float(0)
-        many_params['size_order'] = float(0)
-        many_params['price_position'] = float(0)
-        many_params['size_position'] = float(0)
-        many_params['last'] = False
+    print(f"{parametrs=}")
 
     if not position[int(stream['id']) - 1].start:
-        position[int(stream['id']) - 1].first_start(many_params['balance'], many_params['leverage'], many_params['price_order'], many_params['size_order'], many_params['price_position'], many_params['size_position'], candles[1]['price'])
+        parametrs['balance'] = stream['order']['balance']
+        parametrs['leverage'] = float(0)
+        parametrs['price_order'] = float(0)
+        parametrs['size_order'] = float(0)
+        parametrs['price_position'] = float(0)
+        parametrs['size_position'] = float(0)
+        parametrs['last'] = False
+        position[int(stream['id']) - 1].first_start(parametrs['balance'], parametrs['leverage'], parametrs['price_order'],
+                                                    parametrs['size_order'], parametrs['price_position'], parametrs['size_position'], candles[1]['price'])
 
-    leverage = get_leverage(block, many_params)
+    leverage = get_leverage(block, parametrs)
 
     print(f"{leverage=}")
-    print(f"{candle=}")
+
     if direction == 'long':
         position[int(stream['id']) - 1].update(float(leverage), float(candle['price']))
         params = position[int(stream['id']) - 1].update_long()
     elif direction == 'short':
         position[int(stream['id']) - 1].update(float(leverage), float(candle['price']))
         params = position[int(stream['id']) - 1].update_short()
-    print(f"{position[int(stream['id']) - 1].price_order=}")
+
     params_name = ('balance', 'leverage', 'size_order', 'price_order', 'size_position', 'price_position', 'price',
-                   'pnl', 'equity', 'rpl')
+                   'pnl', 'rpl')
     params_dict = dict(zip(params_name, params))
 
     for k in params_dict:
-        many_params[k] = params_dict[k]
-    many_params['last'] = True
-    many_params['direction'] = direction
-    many_params['order_type'] = block['actions'][0]['order_type']
-    many_params['block_id'] = block['id']
-    print(f"{stream['id']} {many_params=}")
+        parametrs[k] = params_dict[k]
+    parametrs['last'] = True
+    parametrs['direction'] = direction
+    parametrs['order_type'] = block['actions'][0]['order_type']
+    parametrs['block_id'] = block['id']
+    print(f"{stream['id']} {parametrs=}")
 
-    return many_params
+    return parametrs
 
 
 
